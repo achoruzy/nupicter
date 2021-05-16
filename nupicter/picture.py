b42f2ea9d84b19370f0dd169ff1ca4b34651448f
industArk <achoruzy@gmail.com>
2021-05-16 13:51:50 +0200

    0004 add pixels + tests

--- nupicter/picture.py
+++ nupicter/picture.py
@@ -4,8 +4,21 @@
 #   Arkadiusz Choruzy
 #   achoruzy@gmail.com
 
+"""Nupicter's module for editing Picture objects created by FileHandler module.
+
+Picture class handles objects converted to [[[R, G, B], [R, G, B], ...], ...] format
+and includes methods for edition of such files.
+"""
+
+
 class Picture():
 
     def __init__(self, img):
 
-        self.name = 'Picture'
+        self.name = 'Picture Object'
+
+        self.pixels = img
+
+
+if __name__ == '__main__':
+    pass
