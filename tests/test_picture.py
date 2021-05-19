044191253ef742e2c8af0d9c6817c128e0e20622
industArk <achoruzy@gmail.com>
2021-05-19 21:58:04 +0200

    0006 done + tests

--- tests/test_picture.py
+++ tests/test_picture.py
@@ -10,6 +10,9 @@ import nupicter.picture as nupp
 
 import numpy as np
 
+import pytest
+
+
 FILEPATHS = {
     'png': 'C:/Users/achor/Desktop/Python/Projects/002_Nupicter/tests/testfiles/4by4.png'}
 IN_USE = 'png'
@@ -20,7 +23,7 @@ file = nupf.FileHandler(FILEPATHS[IN_USE])
 class TestPicture():
 
     def test_init(self):
-        """ Tests class __init__ . """
+        """Tests if class __init__ works properly and Picture object is well built."""
 
         pic = file.open()
 
@@ -28,35 +31,38 @@ class TestPicture():
         assert pic.pixels[0][0] == [237, 28, 36]
 
     def test_as_array(self):
-        pic = file.open()
+        """Tests if Picture.as_arraty returns proper numpy.array object."""
 
+        pic = file.open()
         pix_array = pic.as_array()
 
         assert type(pix_array) == np.ndarray
         assert list(pix_array[0][0]) == [237, 28, 36]
 
     def test_return_picture(self):
-        pic = file.open()
+        """Tests if Picture.return_picture creates proper Picture object."""
 
+        pic = file.open()
         new_picture_obj = pic.return_picture()
 
         assert type(new_picture_obj) == nupp.Picture
 
-    def test_save(self):
-        pic = file.open()
+    def test_save_exception(self):
+        """Tests exceptions that may occure when Picture.save is used.
+        If all exception tests are handled in test then file creation will work."""
 
-        assert type(pic.save('something', 'filename')) == AttributeError
+        pic = file.open()
+        path = 'C:/Users/achor/Desktop/Python/Projects/002_Nupicter/tests/testfiles/'
+
+        with pytest.raises(Exception):
+            assert pic.save('something', 'filename')
+
+        with pytest.raises(Exception):
+            assert pic.save(path, '$%^aaaaaa_')
+
+        with pytest.raises(Exception):
+            assert pic.save(path, 'savetest', 'XYz')
 
 
 if __name__ == '__main__':
-    # pass
-
-    from PIL import Image
-    import numpy as np
-
-    im = Image.open(FILEPATHS[IN_USE])
-    a = np.asarray(im)
-
-    img = Image.fromarray(a)
-
-    print(a[0][0])
+    pass
